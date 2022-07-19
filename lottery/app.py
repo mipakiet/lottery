000f53f0dbc736b3c7e8668293141c003a9fc50f
Mikolaj Pakiet (PGS Software) <mipakiet@pgs-soft.com>
2022-07-19 23:03:42 +0200

    upgrade python style

--- lottery/app.py
+++ lottery/app.py
@@ -4,8 +4,38 @@ import json
 import csv
 
 
+def get_file(permitted_extensions: tuple) -> pathlib.Path:
+
+    file = pathlib.Path()
+    first_time = True
+
+    while first_time or not file.exists():
+        if first_time:
+            first_time = False
+        else:
+            print("Wrong file path. Try again.")
+
+        path = input("Please enter the path to the data file [data/] - ")
+        if path == "":
+            path = "data/"
+        name = input("Name of the file [participants1] - ")
+        if name == "":
+            name = 'participants1'
+        extension = input("Extension of the file[only json and csv are acceptable][csv] - ")
+        if extension == "":
+            extension = 'csv'
+        while extension not in permitted_extensions:
+            print("Wrong extension")
+            extension = input("Extension of the file[only json and csv are acceptable] - ")
+
+        file_direction = path + name + '.' + extension
+        file = pathlib.Path(file_direction)
+
+    return file
+
+
 class Participant:
-    def __init__(self, first_name, second_name, weight=1):
+    def __init__(self, first_name: str, second_name: str, weight=1):
         self.first_name = first_name
         self.second_name = second_name
         self.weight = weight
@@ -23,27 +53,8 @@ class Lottery:
         self.participant = []
 
     def load_participants(self):
-        def get_file_path():
-            path = input("Please enter the path to the data file [data/] - ")
-            if path == "":
-                path = "data/"
-            name = input("Name of the file [participants1] - ")
-            if name == "":
-                name = 'participants1'
-            extension = input("Extension of the file[only json and csv are acceptable][csv] - ")
-            if extension == "":
-                extension = 'csv'
-            while extension not in ["csv", "json"]:
-                print("Wrong extension")
-                extension = input("Extension of the file[only json and csv are acceptable] - ")
 
-            file_direction = path + name + '.' + extension
-            return pathlib.Path(file_direction)
-
-        file = get_file_path()
-        while not file.exists():
-            print("File not exist. Let's try again")
-            get_file_path()
+        file = get_file(("csv", "json"))
 
         if file.suffix == ".csv":
             with open(file, mode='r') as csv_file:
@@ -52,21 +63,26 @@ class Lottery:
                 for row in csv_reader:
                     if line_count != 0:
                         if len(row) < 3:
-                            print("Wrong csv format for this app")
+                            print("Wrong csv content for this app")
                             return False
                         if len(row) > 3:
                             try:
                                 weight = int(row[3])
                             except ValueError:
-                                print("Wrong csv format for this app")
+                                print("Wrong csv content for this app")
                                 return False
                             self.participant.append(Participant(row[1], row[2], weight))
                         else:
                             self.participant.append(Participant(row[1], row[2]))
 
                     line_count += 1
-
+        elif file.suffix == ".json":
+            pass
+        else:
+            print('strange?')
 
     def run(self):
+        print('-' * 60)
         print("HI! Welcome to the lottery!")
+        print('-' * 60)
         self.load_participants()
