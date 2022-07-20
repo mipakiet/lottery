40e59d9c8ea84d786fa7e6f4bb3dd4f3b76b7075
Mikolaj Pakiet (PGS Software) <mipakiet@pgs-soft.com>
2022-07-20 15:54:23 +0200

    add json loading

--- lottery/app.py
+++ lottery/app.py
@@ -4,36 +4,6 @@ import json
 import csv
 
 
-def get_file(permitted_extensions: tuple) -> pathlib.Path:
-
-    file = pathlib.Path()
-    first_time = True
-
-    while first_time or not file.exists():
-        if first_time:
-            first_time = False
-        else:
-            print("Wrong file path. Try again.")
-
-        path = input("Please enter the path to the data file [data/] - ")
-        if path == "":
-            path = "data/"
-        name = input("Name of the file [participants1] - ")
-        if name == "":
-            name = 'participants1'
-        extension = input("Extension of the file[only json and csv are acceptable][csv] - ")
-        if extension == "":
-            extension = 'csv'
-        while extension not in permitted_extensions:
-            print("Wrong extension")
-            extension = input("Extension of the file[only json and csv are acceptable] - ")
-
-        file_direction = path + name + '.' + extension
-        file = pathlib.Path(file_direction)
-
-    return file
-
-
 class Participant:
     def __init__(self, first_name: str, second_name: str, weight=1):
         self.first_name = first_name
@@ -54,7 +24,32 @@ class Lottery:
 
     def load_participants(self):
 
-        file = get_file(("csv", "json"))
+        file = pathlib.Path()
+        first_time = True
+
+        while first_time or not file.exists():
+            if first_time:
+                first_time = False
+            else:
+                print("Wrong file path. Try again.")
+
+            path = input("Please enter the path to the data file [data/] - ")
+            if path == "":
+                path = "data/"
+            name = input("Name of the file [participants2] - ")
+            if name == "":
+                name = 'participants2'
+            extension = input("Extension of the file[only json and csv are acceptable][json] - ")
+            if extension == "":
+                extension = 'json'
+            while extension not in ['csv', 'json']:
+                print("Wrong extension")
+                extension = input("Extension of the file[only json and csv are acceptable][json] - ")
+                if extension == "":
+                    extension = 'json'
+
+            file_direction = path + name + '.' + extension
+            file = pathlib.Path(file_direction)
 
         if file.suffix == ".csv":
             with open(file, mode='r') as csv_file:
@@ -63,13 +58,13 @@ class Lottery:
                 for row in csv_reader:
                     if line_count != 0:
                         if len(row) < 3:
-                            print("Wrong csv content for this app")
+                            print("Error with loading data from file - not enough columns")
                             return False
                         if len(row) > 3:
                             try:
                                 weight = int(row[3])
                             except ValueError:
-                                print("Wrong csv content for this app")
+                                print("Error with loading data from file - weight must be integer")
                                 return False
                             self.participant.append(Participant(row[1], row[2], weight))
                         else:
@@ -77,12 +72,22 @@ class Lottery:
 
                     line_count += 1
         elif file.suffix == ".json":
-            pass
-        else:
-            print('strange?')
+            with open(file, 'r') as f:
+                data = json.load(f)
+                for item in data:
+                    if 'first_name' not in item and 'second_name' not in item:
+                        print("Error with loading data from file - wrong variables names")
+                        return False
+                    if 'weight' in item:
+                        self.participant.append(Participant(item['first_name'], item['last_name'], item['weight']))
+                    else:
+                        self.participant.append(Participant(item['first_name'], item['last_name']))
 
     def run(self):
         print('-' * 60)
         print("HI! Welcome to the lottery!")
         print('-' * 60)
-        self.load_participants()
+        if not self.load_participants():
+            return False
+            return False
+
